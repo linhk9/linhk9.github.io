1e463925cfb1c63336332b3c1d9e8c9f5b9ad7bc
Rodrigo Luís <54328638+linhk9@users.noreply.github.com>
2023-10-07 01:35:41 +0100

    Create redirect.ps1

---
+++ win/redirect.ps1
@@ -0,0 +1,15 @@
+# Define the target URL
+$targetURL = "https://raw.githubusercontent.com/linhk9/linhk9.github.io/main/winutil.ps1"
+
+# Send a 301 (permanent) redirect response
+[Microsoft.AspNetCore.Http.StatusCodes]::MovedPermanently
+
+# Set the Location header to the target URL
+$headers = @{
+    "Location" = $targetURL
+}
+
+# Output the headers to perform the redirect
+$headers.GetEnumerator() | ForEach-Object {
+    "$($_.Key): $($_.Value)"
+}
